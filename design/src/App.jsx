a2de4b0dd071abb1428253cc3da05fa1c98bbe49
JuanPUwu <pablys8@gmail.com>
2024-11-10 19:15:49 -0500

    cambios rutas

--- design/src/App.jsx
+++ design/src/App.jsx
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import Frontpage from "./comp/pestañas/portada/front-page.jsx";
 import Login from "./comp/pestañas/login/login.jsx";
 
@@ -6,6 +11,7 @@ export default function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Navigate to="/frontpage" replace />} />
         <Route path="/frontpage" element={<Frontpage />} />
         <Route path="/login" element={<Login />} />
       </Routes>
