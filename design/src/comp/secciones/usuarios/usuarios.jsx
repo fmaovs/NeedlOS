57386956713e886299c9a1c9bc6cee8bdb969b39
JuanPUwu <pablys8@gmail.com>
2025-01-24 20:59:19 -0500

    mao canson

--- design/src/comp/secciones/usuarios/usuarios.jsx
+++ design/src/comp/secciones/usuarios/usuarios.jsx
@@ -1,7 +1,14 @@
+import Encabezado from "../../encabezado-seccion/encabezado.jsx";
+
 export default function Usuarios() {
-    return(
-        <div>
-            Usuarios
-        </div>
-    )
+  return (
+    <Encabezado
+      titEncabezado={"Usuarios"}
+      conBtCrear={""}
+      opc1={"Cliente"}
+      opc2={"Prenda"}
+      opc3={"Valor"}
+      opc4={"N°"}
+    />
+  );
 }
